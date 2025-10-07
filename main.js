7f85e675d51f1bf21476b917d53298731f2e71f0
jonassantanademetreo <145785837+jonassantanademetreo@users.noreply.github.com>
2025-10-07 12:02:37 -0300

    jonas

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-
-
-
-
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPergunta = document.querySelector(".caixa-pergunta");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {

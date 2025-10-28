5703c298de750af60e8d446b194441b5a156d565
jonassantanademetreo <145785837+jonassantanademetreo@users.noreply.github.com>
2025-10-28 12:16:38 -0300

    kojnuibujyhb

--- main.js
+++ main.js
@@ -1,107 +1,115 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPergunta = document.querySelector(".caixa-pergunta");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Pergunta?",
+        enunciado: "Quando foi desenvolvido a primeira copa do mundo?",
         alternativas: [
             {
-                texto: " Texto/resposta",
-                afirmacao: " Afirmação ",
+                texto: " 13 de julho de 1930, no uruguai.",
+                afirmacao: " criada em 1930 no uruguai. ",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " criada  13 de julho de 1934, no méxico. ",
+                afirmacao: " criada no dia 13 de julho. ",
             }
         ]
     },
     {
-        enunciado: " Pergunta?",
+        enunciado: " Qual era o nome da taça antiga?",
         alternativas: [
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Até 1970, o troféu era conhecido como Taça Jules Rimet.  ",
+                afirmacao: " Criador da Copa Jules Rimet. ",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Taça das Nações Unidas ",
+                afirmacao: " Liga das Nações da UEFA ",
             }
         ]
     },
     {
-        enunciado: " Pergunta?",
+        enunciado: " Qual país foi o anfitrião da Copa do Mundo de 2014? ",
         alternativas: [
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Brasil ",
+                afirmacao: " O Brasil sediou a Copa do Mundo de 2014. ",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Argentina ",
+                afirmacao: " A Argentina sediou a Copa do Mundo de 2014. ",
             }
         ]
     },
     {
-        enunciado: " Pergunta?",
+        enunciado: " Qual país sediou a Copa do Mundo de 2022? ",
         alternativas: [
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Catar   ",
+                afirmacao: " O Catar sediou a Copa do Mundo de 2022. ",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Russia   ",
+                afirmacao: " O Russia sediou a Copa do Mundo de 2022. ",
             }
         ]
     },
     {
-        enunciado: " Pergunta?",
+        enunciado: " Quantos títulos o Brasil tem na Copa do Mundo?   ",
         alternativas: [
             {
-                texto: " Texto/resposta ",
-                afirmacao: "Afirmação",
+                texto: " 5 Titulos ",
+                afirmacao: "O Brasil é o país com mais títulos, tendo vencido a Copa do Mundo 5 vezes. ",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " 4 Titulos ",
+                afirmacao: " O Brasil é o país com mais títulos, tendo vencido a Copa do Mundo 4 vezes. ",
             }
         ]
     },
 ];
+
+
 let atual = 0;
 let perguntaAtual;
-let historiaFinal="";
+let historiaFinal = " ";
 
-if (atual >=perguntas.length){
-    mostraResultado();
-    return
+function mostraPergunta(){
+
+    if (atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
+
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = " ";
+    mostraAlternativas();
 }
 
-
-    function mostraPergunta(){ 
-        for()
-        perguntaAtual = perguntas[atual]
-        caixasPerguntas.textContent = perguntaAtual.enunciado;
-mostraAlternativas();
-    }
-
-    function mostraAlternativas(){
-        for (const alternativa of perguntaAtual.alternativas){
-            const botaoAlternativas = document.createElement("button");
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click ,"() =>respostaSelecionada(alternativa)){
-          caixaAlternativas.appendChild(botaoAlternativas);  
-        }
-        }
-    }
-    
-    function respostaSelecionada(opcaoSelecionada){
-        const afirmacoes = opcaoSelecionada.afirmacao;
-        historia += afirmacoes+"";
-        atual++
-        mostraPergunta();
-    }
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    } 
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
     mostraPergunta();
+}
+
+function mostraResultado(){
+    caixaPerguntas.textContent = "Se fosse possível traduzir sua forma de aprender em palavras, diríamos que...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = " ";
+}
+
+mostraPergunta();
